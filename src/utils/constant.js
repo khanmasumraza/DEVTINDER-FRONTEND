9e9bc4a80152aac783f6a7c5b4e89def7f264c8f
Khanmasum123 <bcamasum@gmail.com>
2025-12-08 20:49:31 +0530

    url chnag

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,1 @@
-const isLocalhost =
-  window.location.hostname === 'localhost' ||
-  window.location.hostname === '127.0.0.1'
-
-export const BASE_URL = isLocalhost
-  ? 'http://localhost:7777' // Local backend
-  : 'http://16.170.224.154' // EC2 backend
+export const BASE_URL = 'http://localhost:7777'
